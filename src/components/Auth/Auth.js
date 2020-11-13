c166858940d39d561bfe7af3130c2356b2dc509e
Katie <adlerk8@gmail.com>
2020-11-13 09:42:02 -0700

    in progress - building React

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Auth extends Component {
+    render() {
+        return (
+            <div>This is the Auth component</div>
+        )
+    }
+}
+
+export default Auth;
